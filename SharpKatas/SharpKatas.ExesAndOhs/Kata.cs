34bec038857014b02797d37159642095bf771237
Alexey Maltsev <aamaltsev@skbkontur.ru>
2017-12-05 19:45:49 +0500

    My ExesAndOhs solution

---
+++ SharpKatas/SharpKatas.ExesAndOhs/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace SharpKatas.ExesAndOhs
+{
+    public static class Kata
+    {
+        public static bool XO(string input)
+        {
+            var stats = input
+                .ToLower()
+                .Where(c => c == 'x' || c == 'o')
+                .Aggregate(new Tuple<int, int>(0, 0), (tuple, c) =>
+                {
+                    if (c == 'x')
+                        return Tuple.Create(tuple.Item1 + 1, tuple.Item2);
+                    return Tuple.Create(tuple.Item1, tuple.Item2 + 1);
+                });
+            return stats.Item1 == stats.Item2;
+        }
+    }
+}
---
+++ SharpKatas/SharpKatas.ExesAndOhsTests/KataTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using SharpKatas.ExesAndOhs;
+
+namespace SharpKatas.ExesAndOhsTests
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(true, Kata.XO("xo"));
+            Assert.AreEqual(true, Kata.XO("xxOo"));
+            Assert.AreEqual(false, Kata.XO("xxxm"));
+            Assert.AreEqual(false, Kata.XO("Oo"));
+            Assert.AreEqual(false, Kata.XO("ooom"));
+        }
+    }
+}
